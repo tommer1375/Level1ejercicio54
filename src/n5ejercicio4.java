022a1c54dceeb5273c5725c09bdd5dd180c0521a
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-02-05 12:49:13 +0100

    Update n5ejercicio4.java

--- src/n5ejercicio4.java
+++ src/n5ejercicio4.java
@@ -4,7 +4,11 @@ import java.io.*;
 public class n5ejercicio4 {
     public static void main(String[] args) {
 
-        leerDirectorio.leerArchivoTxt("C:\\InformacionDirectorio.txt");
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Introduce la ruta del archivo a leer:");
+        String datoString = myObj.nextLine();
+        leerDirectorio.leerArchivoTxt(datoString);
 
     }
 }
